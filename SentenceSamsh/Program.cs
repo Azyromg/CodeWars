e38e094dacd39ac19c0c8a30cc4d9236b4d7bee8
Azyromg <136343006+Azyromg@users.noreply.github.com>
2023-06-19 16:20:21 +0300

    Добавили проект SentenceSamsh

---
+++ SentenceSamsh/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] words = { "hello", "amazing", "world" };
+
+Console.WriteLine(Smash(words));
+static string Smash(string[] words)
+{
+    string result = words[0];
+    for (int i = 1; i < words.Length; ++i)
+    {
+        result += " " + words[i];
+    }
+    return result;
+}
